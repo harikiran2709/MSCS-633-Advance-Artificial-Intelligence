2282ed5bbb3da7725c44865eedd55d551a96b0cf
harikiran2709 <hrommala41521@ucumberlands.edu>
2025-07-20 20:26:29 -0500

    Add comments

--- Assignment_QR_Code_Generator/qr_generator.py
+++ Assignment_QR_Code_Generator/qr_generator.py
@@ -2,11 +2,11 @@ import qrcode
 
 def generate_qr(url: str, output_file: str = 'qrcode.png') -> None:
     """
-    Generates a QR code for the given URL and saves it as an image file.
+    This function generates a QR code for the given URL and saves it as an png file.
 
-    Args:
-        url (str): The URL to encode in the QR code.
-        output_file (str): The filename for the saved QR code image.
+    Arguments we pass to the function:
+        url (str): The URL link.
+        output_file (str): The filename for the output image.
     """
     # Create qr code instance
     qr = qrcode.QRCode(
@@ -21,7 +21,7 @@ def generate_qr(url: str, output_file: str = 'qrcode.png') -> None:
     # Create an image from the QR Code instance
     img = qr.make_image(fill_color="black", back_color="white")
     img.save(output_file)
-    print(f"QR code generated and saved as {output_file}")
+    print(f"QR code generated for the given url and saved as {output_file}")
 
 if __name__ == "__main__":
     url = input("Enter the URL to generate QR code: ")
